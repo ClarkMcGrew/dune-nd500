6dd062e800dbcbdc404776e76e803999a820b265
Clark McGrew <clark.mcgrew@stonybrook.edu>
2017-04-12 19:25:12 -0400

    Use environment-modules for setting up the releases.

--- setup.sh
+++ setup.sh
@@ -23,11 +23,17 @@
 
 if [ "x${BASH_VERSION}" = "x" ]; then
     echo This must be run using bash
-    exit 1
+    return 1
 fi
 
 ___release_candidate=$1
 shift
+
+if [ "x$___release_candidate" = "x-h" ]; then
+    echo "usage: ndx-setup <release-version>"
+    echo "   The default is the highest version found."
+    return 1
+fi
 
 export NDX_SPACK_ROOT
 NDX_SPACK_ROOT=$(dirname $(realpath ${BASH_SOURCE}))
@@ -47,7 +53,70 @@ source ${NDX_SPACK_ROOT}/spack/share/spack/setup-env.sh
 # Setup the alias to rerun this setup.
 alias ndx-setup="source ${NDX_SPACK_ROOT}/setup.sh"
 
-# Define some local worker functions to help set paths
+# Define the environment modules.
+MODULESHOME=$(spack location --install-dir environment-modules)/Modules
+source ${MODULESHOME}/init/bash
+source ${MODULESHOME}/init/bash_completion
+
+# Make sure the environment is clean.
+module purge
+
+# Check to see if the requested release exists.  The sed command is
+# doing backflips to remove the spack pretty printing.
+if [ ${#___release_candidate} != 0 ]; then
+    ___release_candidate=$(spack find release \
+				  | sed 's/\x1B[[:print:]]*[mGK]//g' \
+				  | tail +2 \
+				  | grep ${___release_candidate} \
+				  | sort \
+				  | tail -1)
+fi
+
+# If there isn't a release, find a default. The sed command is doing
+# backflips to remove the spack pretty printing.
+if [ ${#___release_candidate} == 0 ]; then
+    ___release_candidate=$(spack find release \
+				  | sed 's/\x1B[[:print:]]*[mGK]//g' \
+				  | tail +2 \
+				  | sort \
+				  | tail -1)
+fi
+
+# If there still isn't a release, print a message and stop.
+if [ ${#___release_candidate} == 0 ]; then
+    echo No release candidate found.
+    return 0
+fi
+
+echo Setting up environment for ${___release_candidate}
+
+LOCAL_ENVIRONMENT=$(mktemp)
+spack module loads -r ${___release_candidate} > ${LOCAL_ENVIRONMENT}
+. ${LOCAL_ENVIRONMENT}
+rm ${LOCAL_ENVIRONMENT}
+
+# Try to setup geant4.  GEANT4 installs geant4.sh in the bin directory
+# to setup the environment.  The first "geant4.sh" in the path will
+# define the geant that is used.  This gives the G4 build script the
+# ability to make sure all of the G4 environment variables are
+# correctly defined.
+. geant4.sh >& /dev/null
+if [ $? != 0 ]; then
+    echo GEANT not available.
+fi
+
+# Try to setup root.  ROOT installs thisroot.sh in the bin directory
+# to setup the environment.  This gives the ROOT build script the
+# ability to make sure all of the ROOT environment variables are
+# correctly defined.
+. thisroot.sh >& /dev/null
+if [ $? != 0 ]; then
+    echo ROOT not available.
+fi
+
+unset -v ___release_candidate
+
+# Add the scripts.
 ___path_remove ()  {
     export $1=$(eval echo -n \$$1 | \
 	awk -v RS=: -v ORS=: '$0 != "'$2'"' | \
@@ -55,69 +124,21 @@ ___path_remove ()  {
 }
 ___path_append ()  {
     ___path_remove $1 $2
-    eval export $1="\$$1:$2"
+    if [ -d ${2} ]; then
+	eval export $1="\$$1:$2"
+    fi
 }
 ___path_prepend () {
     ___path_remove $1 $2
-    eval export $1="$2:\$$1"
+    if [ -d ${2} ]; then
+	eval export $1="$2:\$$1"
+    fi
 }
 
-# Add the scripts directory to the path.
-___path_prepend PATH ${NDX_SPACK_ROOT}/scripts
+# Update the environment.
+___path_prepend PATH "${NDX_SPACK_ROOT}/scripts"
 
-# Remove the local worker functions from the definitions.
 unset -f ___path_append
 unset -f ___path_prepend
 unset -f ___path_remove
 
-# Now see if we should setup a particular release
-if [ ! -d ${NDX_SPACK_ROOT}/releases ]; then
-    echo No releases installed
-    unset -v ___release_candidate
-    return
-fi
-
-if [ ${#___release_candidate} == 0 ]; then
-    ___release_candidate=$( \
-	find ${NDX_SPACK_ROOT}/releases -mindepth 1 -maxdepth 1 \
-	| sort \
-	| grep $(spack arch) \
-	| tail -1)
-	       
-else
-    ___release_candidate=$(\
-	find ${NDX_SPACK_ROOT}/releases -mindepth 1 -maxdepth 1 \
-	| sort \
-	| grep ${___release_candidate} \
-	| tail -1)
-fi
-
-if [ ${#___release_candidate} == 0 ]; then
-    echo No release selected
-    unset -v ___release_candidate
-    return
-fi
-
-if [ ! -f ${___release_candidate}/setup.sh ]; then
-    echo No setup file for $(basename ${___release_candidate})
-    unset -v ___release_candidate
-    return 
-fi
-
-source ${___release_candidate}/setup.sh
-
-echo NDX_ROOT setup for $(basename ${NDX_ROOT})
-
-# Because you know you want them...
-. thisroot.sh >& /dev/null
-if [ $? != 0 ]; then
-    echo ROOT is not available
-fi
-
-# Because you know you want them...
-. geant4.sh >& /dev/null
-if [ $? != 0 ]; then
-    echo GEANT4 is not available
-fi
-
-unset -v ___release_candidate
